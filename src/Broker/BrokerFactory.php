b38000b3671897881583bc09922380fa85c9b3bf
Simon Samtleben <simon@samtleben.me>
2016-08-11 21:16:16 +0200

    Add config validation to broker factory

--- src/Broker/BrokerFactory.php
+++ src/Broker/BrokerFactory.php
@@ -11,18 +11,42 @@ class BrokerFactory
         $this->brokerConfig = $brokerConfig;
     }
 
-    public function create()
+    /**
+     * Creates message-broker client.
+     *
+     * @return BrokerClient
+     * @throws \RuntimeException
+     */
+    public function create() : BrokerClient
     {
+        if (!isset($this->brokerConfig['type'])) {
+            throw new \RuntimeException('Broker type not set in configuration.');
+        }
+        if (!$this->isValidBrokerType($this->brokerConfig['type'])) {
+            throw new \RuntimeException('Invalid broker type set in configuration');
+        }
+        $broker = null;
         switch ($this->brokerConfig['type']) {
             case 'rabbitmq':
                 $broker = new RabbitmqClient;
                 $broker->connect($this->brokerConfig['credentials']);
                 $broker->setCommandQueue($this->brokerConfig['queues']['cmd']);
-                return $broker;
-                break;
-            default:
-                // @todo throw unknown broker exception
                 break;
         }
+        return $broker;
+    }
+
+    /**
+     * Checks if a broker type is supported/valid.
+     *
+     * @param string $brokerType
+     * @return bool
+     */
+    protected function isValidBrokerType(string $brokerType) : bool
+    {
+        $validBrokerTypes = [
+            'rabbitmq',
+        ];
+        return in_array($brokerType, $validBrokerTypes);
     }
 }
